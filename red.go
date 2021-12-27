b0c3f9bc68f312b70a5b96930d4087bc727c00b4
Percee Goings <percee@pghq.app>
2021-12-26 20:06:26 -0600

    Run persistance worker less frequent

--- red.go
+++ red.go
@@ -17,7 +17,7 @@ import (
 
 const (
 	// Version of the queue
-	Version = "0.0.19"
+	Version = "0.0.20"
 
 	// Prefix is the name prefix of the queue
 	Prefix = "go-red/v" + Version
@@ -178,7 +178,7 @@ func New(redisURL string) *Red {
 	}
 
 	q.scheduler = NewScheduler(&q)
-	q.worker = NewWorker()
+	q.worker = NewWorker().Every(100 * time.Millisecond)
 	return &q
 }
 
